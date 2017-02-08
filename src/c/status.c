04bbe5724d0e6fbb9a7f0ccd6b25ef7b56141521
GreenHex <GreenHex@gmail.com>
2017-02-08 20:17:53 +0530

    Removed name display

--- src/c/status.c
+++ src/c/status.c
@@ -25,7 +25,8 @@ static void status_text_layer_update_proc( Layer *layer, GContext *ctx ) {
   date_window_bounds.origin.x += STATUS_TEXT_HOR_ADJ;
   date_window_bounds.origin.y -= STATUS_TEXT_VER_ADJ;
   graphics_context_set_text_color( ctx, PBL_IF_COLOR_ELSE( GColorArmyGreen, GColorBlack ) /* background_colour */ );
-  strftime( date_str, sizeof( date_str ), show_name?"KAVAN":DATE_STRING, &tm_time );
+  // strftime( date_str, sizeof( date_str ), show_name?"KAVAN":DATE_STRING, &tm_time );
+  strftime( date_str, sizeof( date_str ), DATE_STRING, &tm_time );
   if ( date_str[3] == date_str[4] ) { // remove double spaces between day and single digit date 
     memmove( &date_str[4], &date_str[5], sizeof( date_str ) - 4 );
   }
